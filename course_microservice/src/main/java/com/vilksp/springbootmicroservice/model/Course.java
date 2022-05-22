bd22a247318a717545167dafb1c3c422bca2f6cc
vilksp <v.kasperavicius1@gmail.com>
2022-05-22 12:57:48 +0300

    Course model defined

---
+++ course_microservice/src/main/java/com/vilksp/springbootmicroservice/model/Course.java
@@ -0,0 +1,33 @@
+package com.vilksp.springbootmicroservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "course")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 100, nullable = false)
+    private String subtitle;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDate createTime;
+}
