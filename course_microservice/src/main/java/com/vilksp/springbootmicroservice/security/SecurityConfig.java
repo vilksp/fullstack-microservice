9976a863e15b7fdce13d032ed0fa799ca7a7455d
vilksp <v.kasperavicius1@gmail.com>
2022-05-28 22:20:29 +0300

    simple security

--- course_microservice/src/main/java/com/vilksp/springbootmicroservice/security/SecurityConfig.java
+++ course_microservice/src/main/java/com/vilksp/springbootmicroservice/security/SecurityConfig.java
@@ -1,2 +1,34 @@
-package com.vilksp.springbootmicroservice.security;public class SecurityConfig {
+package com.vilksp.springbootmicroservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        super.configure(http);
+        http.csrf().disable();
+    }
 }
