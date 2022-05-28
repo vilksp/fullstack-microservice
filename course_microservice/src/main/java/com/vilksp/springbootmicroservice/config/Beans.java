2014d508d3f84c7b3960bfd4ca182b6257be5a94
vilksp <v.kasperavicius1@gmail.com>
2022-05-28 22:20:17 +0300

    removed private keyword, cause bean cannot be private or final

--- course_microservice/src/main/java/com/vilksp/springbootmicroservice/config/Beans.java
+++ course_microservice/src/main/java/com/vilksp/springbootmicroservice/config/Beans.java
@@ -1,2 +1,15 @@
-package com.vilksp.springbootmicroservice.config;public class Beans {
+package com.vilksp.springbootmicroservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class Beans {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
