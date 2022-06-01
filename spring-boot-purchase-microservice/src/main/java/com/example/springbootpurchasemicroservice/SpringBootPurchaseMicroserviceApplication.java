cba330ae66f58a1d41568d8e0694287708d06d3c
vilksp <v.kasperavicius1@gmail.com>
2022-06-01 21:48:15 +0300

    initial commit  of purchase microservice

---
+++ spring-boot-purchase-microservice/src/main/java/com/example/springbootpurchasemicroservice/SpringBootPurchaseMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpurchasemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPurchaseMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPurchaseMicroserviceApplication.class, args);
+	}
+
+}
---
+++ spring-boot-purchase-microservice/src/test/java/com/example/springbootpurchasemicroservice/SpringBootPurchaseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpurchasemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPurchaseMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
